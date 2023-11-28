175526d9c1c9e2c948b1bda5e6daf54746cbfc4c
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-28 15:07:36 +0100

    scaffolding

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class="text-center">PHP SNACK</h1>
+    <main class="container-fluid d-flex">
+        <section id="snak-1" class="bg-primary w-100 text-center">SNACK 1</section>
+        <section id="snak-2" class="bg-warning w-100 text-center">SNACK 2</section>
+        <section id="snak-3" class="bg-danger w-100 text-center">SNACK 3</section>
+    </main>
+</body>
+
+</html>
